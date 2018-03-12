c50cb620cd958449902bc8e110d97d78fa5ec150
Suresh Raavi <raavi.usa@gmail.com>
2018-03-12 13:07:55 -0700

    Updated the file

--- Azure-RebootSingleInstance.ps1
+++ Azure-RebootSingleInstance.ps1
@@ -1,5 +1,8 @@
-﻿
+﻿#Login to your Azure Account
 Login-AzureRmAccount
+
+#Set the context to the Subscription where your service is hosted
 Set-AzureRmContext 
 
+#Run the Reset-AzureRoleInstance command
 ReSet-AzureRoleInstance -ServiceName "YourAppServiceName" -Slot production -InstanceName "YourAppServiceInstanceName" -Reboot
